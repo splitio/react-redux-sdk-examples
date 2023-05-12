2198882642faa2cb08175945a71dfe0b69547eac
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-05-12 16:51:30 -0300

    polishing

--- src/components/ListOfFeatures.js
+++ src/components/ListOfFeatures.js
@@ -4,37 +4,37 @@ import { connectSplit, selectTreatmentValue, getSplitNames } from '@splitsoftwar
 /**
  * This example shows `connectSplit` connector.
  *
- * The following components print a list of splits (features) with their corresponting
- * treatment values (flags). The list of splits is obteined with `getSplitNames` function,
+ * The following components print a list of feature flags with their corresponting
+ * treatment values (flags). The list of feature flags is obteined with `getSplitNames` function,
  * and treatment values are accessed from the state with `selectTreatmentValue` selector.
  * While the SDK is not ready, it prints a simple loading message.
  */
 
-function SplitItem({ splitName, treatmentValue }) {
+function FeatureItem({ featureFlagName, treatmentValue }) {
   return (
     <div>
-      <h4>{`Split: ${splitName}`}</h4>
+      <h4>{`Feature flag: ${featureFlagName}`}</h4>
       <p>{`Treatment value: ${treatmentValue}`}</p>
     </div>
   )
 }
 
-function ListOfSplits(props) {
+function ListOfFeatures(props) {
   const { splitio } = props;
-  const splitNames = getSplitNames();
+  const featureFlagNames = getSplitNames();
 
   return (
-    /* We print the list of Splits once the SDK is ready. */
+    /* We print the list of feature flags once the SDK is ready. */
     splitio.isReady ?
       <div>{
-        splitNames.map(splitName =>
-          <SplitItem key={splitName}
-            splitName={splitName}
-            treatmentValue={selectTreatmentValue(splitio, splitName)}
+        featureFlagNames.map(featureFlagName =>
+          <FeatureItem key={featureFlagName}
+            featureFlagName={featureFlagName}
+            treatmentValue={selectTreatmentValue(splitio, featureFlagName)}
           />)
       }</div> :
       <p>Loading Split SDK ...</p>
   )
 }
 
-export default connectSplit()(ListOfSplits);
+export default connectSplit()(ListOfFeatures);
